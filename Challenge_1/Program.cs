56ebe366f20492bef4eaf33a42c8c74aed80d3df
Kingkonbro <konradboruch735@gmail.com>
2025-09-28 19:31:09 +0100

    Completed

--- Challenge_1/Program.cs
+++ Challenge_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace Challenge_1
     {
         static void Main(string[] args)
         {
+            // The user is asked to enter the amount of faces on the dice, then this vaule is stored in the variable 'faces'
+            Console.Write("Enter the number of faces on the dice: ");
+            int faces = Convert.ToInt32(Console.ReadLine());
+
+            // A random number is generated 
+            Random rand = new Random();
+            int r = rand.Next(1, (faces + 1));
+
+            // The result is printed to the user
+            Console.WriteLine($"The result of the dice roll is: {r}");
+
         }
     }
 }
