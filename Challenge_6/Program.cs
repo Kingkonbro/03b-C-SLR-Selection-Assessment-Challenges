7573cc5c41b170de2551fce3a91fb87803ec71b2
Kingkonbro <konradboruch735@gmail.com>
2025-09-30 20:47:53 +0100

    Completed

---
+++ Challenge_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The user is prompted to enter the nitrate level
+            Console.Write("Enter nitrate level: ");
+            float nitrate = Convert.ToSingle(Console.ReadLine());
+
+            string dose = "";
+            if (nitrate > 10)
+            {
+                dose = "3ml";
+            }
+
+            else if (nitrate > 2.5)
+            {
+                dose = "2ml";
+            }
+
+            else if (nitrate > 1)
+            {
+                dose = "1ml";
+            }
+
+            else if (nitrate <= 1)
+            {
+                dose = "0.5ml";
+            }
+
+            Console.WriteLine($"The dose is: {dose}");
+        }
+    }
+}
